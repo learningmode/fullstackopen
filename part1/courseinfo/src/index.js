11c9ee9be45a1791eacff6b91267376337f9c0f1
learningmode <learningmode151@gmail.com>
2020-12-31 01:24:50 +0530

    my experimenting with prop objects

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,26 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const App =()=>{
+  let item ='';
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+function printMe(item){
+  return <p>{item.name}</p>
+}
  return(
       <div>
         <h1>{course}</h1>
-        <p> {part1.name} {part1.exercises}</p>
-        <p>{part2.name} {part2.exercises}</p>
-        <p>{part3.name} {part3.exercises}</p>
+        <p>{parts[0].name}</p>
+        {parts.map(function(item){
+           return<p>{item.name}</p>
+        })}
+        <p>Using ForEach</p>
+        {parts.forEach(printMe)}
       </div>
   )
 }
